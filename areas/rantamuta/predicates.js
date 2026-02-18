6f7ad15066d18710955f2062f9352182d1cad60f
Rendall <rendall@gmail.com>
2026-02-18 19:40:28 +0200

    Add perception gallery

--- areas/rantamuta/predicates.js
+++ areas/rantamuta/predicates.js
@@ -49,7 +49,62 @@ function basinRunesGlowing(q) {
   );
 }
 
+/**
+ * @param {*} q
+ * @returns {boolean}
+ */
+function hasBronzeClapperInBell(q) {
+  if (!q || typeof q.roomContainerHasItem !== 'function') {
+    return false;
+  }
+
+  return q.roomContainerHasItem(
+    'rantamuta:bell_belfry',
+    'rantamuta:crackedBell',
+    'rantamuta:bronzeClapper'
+  );
+}
+
+/**
+ * @param {*} q
+ * @returns {boolean}
+ */
+function hasWaxSealInReliquary(q) {
+  if (!q || typeof q.roomContainerHasItem !== 'function') {
+    return false;
+  }
+
+  return q.roomContainerHasItem(
+    'rantamuta:bell_nave',
+    'rantamuta:reliquary',
+    'rantamuta:waxSeal'
+  );
+}
+
+/**
+ * @param {*} q
+ * @returns {boolean}
+ */
+function hasResonantShardInGallery(q) {
+  if (!q || typeof q.roomHasItem !== 'function') {
+    return false;
+  }
+
+  return q.roomHasItem('rantamuta:perception_gallery', 'rantamuta:resonantShard');
+}
+
 module.exports = {
   is_slab_open: ({ q }) => isDescentOpen(q),
   is_basin_runes_glowing: ({ q }) => basinRunesGlowing(q),
+  is_gallery_feature_enabled: ({ q }) =>
+    !!(q && typeof q.areaFlag === 'function' && q.areaFlag('rantamuta', 'galleryFeatureEnabled')),
+  is_gallery_mirrors_awake: ({ q }) =>
+    !!(q && typeof q.roomFlag === 'function' && q.roomFlag('rantamuta:perception_gallery', 'mirrorsAwake')),
+  is_resonant_shard_in_gallery: ({ q }) => hasResonantShardInGallery(q),
+  does_viewer_hold_resonant_shard: ({ q }) =>
+    !!(q && typeof q.actorHasItem === 'function' && q.actorHasItem('rantamuta:resonantShard')),
+  is_wax_seal_in_reliquary: ({ q }) => hasWaxSealInReliquary(q),
+  is_bronze_clapper_in_bell: ({ q }) => hasBronzeClapperInBell(q),
+  is_gallery_harmonic: ({ q }) =>
+    isDescentOpen(q) && hasBronzeClapperInBell(q) && hasWaxSealInReliquary(q),
 };
