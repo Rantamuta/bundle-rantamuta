e58b0199e8d1fbd0fb1cda3cacdcfae09c670015
Rendall <rendall@gmail.com>
2026-02-21 16:40:17 +0200

    Set replaceSuccess in exit flavor plan

--- areas/rantamuta/scripts/items/facadeDoor.js
+++ areas/rantamuta/scripts/items/facadeDoor.js
@@ -115,6 +115,9 @@ module.exports = {
             }
 
             return {
+              renderPolicy: {
+                replaceSuccess: true,
+              },
               render: {
                 messages: [
                   {
